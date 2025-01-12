74a4dfd436b79547c58c6076e4c910dd44ff1298
Evens88 <istvanfahar@gmail.com>
2025-01-12 13:18:08 -0500

    Merging Lowrider to 2025
    Errors
    
    - Revrobotics libraries missing from InTakeSubystem
    
    - Errors related to class CommandSwereDrivetrain and TunerConstants in
     DriveToLocationCommand, DriveToTrapCommand, and visionSubsystem are due to 2024 classes/methods missing from the newly generated CommandSwereDrivetrain
    
    - Errors related to Pathplanner in DriveToTrapCommand
    
    - Errors related to Photovision in visionSubsystem
    
    - Unknown error (Ln 21) in shooterSubsystem
    
    - Errors in Robot.java related to classes/methods missing from last year's RobotContainer

--- LowRider_questNav-Imported/src/main/java/frc/robot/util/log/Logger.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.util.log;
-
-public interface Logger {
-    public void suspend();
-	public void resume();
-	public boolean isSuspended();
-    public void add(Object value);
-    public void flush();
-}
