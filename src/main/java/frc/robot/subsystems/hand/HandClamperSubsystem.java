9d8929b0d92625c54fecd3600efef1e2abfbc041
Evens88 <istvanfahar@gmail.com>
2025-02-08 18:14:47 -0500

    Cleaning out constants

--- src/main/java/frc/robot/subsystems/hand/HandClamperSubsystem.java
+++ src/main/java/frc/robot/subsystems/hand/HandClamperSubsystem.java
@@ -10,7 +10,6 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.state.StateMachineCallback;
 import frc.robot.state.score.ScoreInput;
 import frc.robot.state.score.constants.PositionConstants;
