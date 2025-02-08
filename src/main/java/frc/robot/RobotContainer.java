333d20f86a094a271ea6b59dd84809a988a46c80
Christopher Byrum <contact@ansibly.com>
2025-02-08 16:33:23 -0500

    Small code formatting change

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -227,7 +227,7 @@ public class RobotContainer {
     }));
     
     opBack.whileTrue(new InstantCommand(() -> visionSubsystem.setConfidence(true)))
-    .onFalse(new InstantCommand(() -> visionSubsystem.setConfidence(false)));
+      .onFalse(new InstantCommand(() -> visionSubsystem.setConfidence(false)));
 
     driveSubsystem.registerTelemetry(logger::telemeterize);
   }
