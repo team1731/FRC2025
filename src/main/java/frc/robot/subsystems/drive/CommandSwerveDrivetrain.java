5dcf5e85b5baf0d0e5a2762e50a39954b9d8b897
Brent Lepke <bdleppke@gmail.com>
2025-04-11 16:51:34 -0400

    Turn Telemetry back on

--- src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/CommandSwerveDrivetrain.java
@@ -140,7 +140,7 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements To
             vslamSubsystem = new VSLAMSubsystem(visionCallback);
             vslamSubsystem.configure();
         }
-        SignalLogger.enableAutoLogging(false);
+        SignalLogger.enableAutoLogging(true);
     }
 
     public CommandSwerveDrivetrain(boolean enabled, SwerveDrivetrainConstants driveTrainConstants,
